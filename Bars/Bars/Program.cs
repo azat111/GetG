a2aba2d7605c37baa55f1d2fcfdbaa7e4a79ca45
azat111 <82712722+azat111@users.noreply.github.com>
2022-03-23 23:42:41 +0300

    m

---
+++ Bars/Bars/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bars
+{
+    public class Delegate
+    {
+        public event EventHandler<char>? OnKeyPressed;
+
+        public void Run()
+        {
+            char c = Console.ReadKey().KeyChar;
+            while (c != 'c' && c != 'с')
+            {
+                OnKeyPressed?.Invoke(this, c);
+                c = Console.ReadKey().KeyChar;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var read = new Delegate();
+            read.OnKeyPressed += Sym;
+            read.Run();
+        }
+        public static void Sym(object? sender, char c)
+        {
+            Console.WriteLine(c);
+        }
+
+    }
+    
+}
